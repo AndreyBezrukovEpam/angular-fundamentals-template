fcd796e76fc34521b2fbcbffb9442d629a6c8da1
AndreyBezrukov <andrey_bezrukov@epam.com>
2024-07-25 20:00:33 +0200

    Course form started

--- src/app/shared/components/course-form/course-form.component.ts
+++ src/app/shared/components/course-form/course-form.component.ts
@@ -11,6 +11,7 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './course-form.component.html',
   styleUrls: ['./course-form.component.scss'],
 })
+
 export class CourseFormComponent implements OnInit 
 {
   constructor(public fb: FormBuilder, public library: FaIconLibrary) {
@@ -19,15 +20,33 @@ export class CourseFormComponent implements OnInit
   courseForm!: FormGroup;
   // Use the names `title`, `description`, `author`, 'authors' (for authors list), `duration` for the form controls.
 
+  courseData = {'title': '', 'description':'', 'author':'', 'authors':[], 'duration':''};
+
   ngOnInit()
   {
     this.courseForm = new FormGroup({
       'title': new FormControl(null, Validators.required),
       'description': new FormControl(null, [Validators.required]),
       'author': new FormControl(null, Validators.required),
-      'authors': new FormArray([]),
+      'authors': new FormControl([]),
       'duration': new FormControl(null, Validators.required),
     });
   }
 
+  onClick(){
+    console.log(this.courseForm.get('duration'));
+  }
+
+  onAddAuthor():void {
+    this.courseForm.controls['authors'].value.push(this.courseForm.controls['author'].value);
+  }
+
+  getAuthors(): string {
+    return this.courseForm.controls['authors'].value.join(', ');
+  }
+
+  getDuration(): string {
+    const v: string = this.courseForm.controls['duration'].value;
+    return v ? v : "";
+  }
 }
