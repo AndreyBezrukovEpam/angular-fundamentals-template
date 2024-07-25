63aee356e2a58e8dee4e776f076cbdb32b079b4c
AndreyBezrukov <andrey_bezrukov@epam.com>
2024-07-25 17:56:31 +0200

    Td and r done

--- src/app/shared/components/registration-form/registration-form.component.ts
+++ src/app/shared/components/registration-form/registration-form.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.scss'],
 })
-export class RegistrationFormComponent {
-  registrationForm!: FormGroup;
+export class RegistrationFormComponent implements OnInit 
+{
+  registrationForm: FormGroup = new FormGroup([]);
   // Use the names `name`, `email`, `password` for the form controls.
+
+  ngOnInit()
+  {
+    this.registrationForm = new FormGroup({
+      'name': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, Validators.required),
+    });
+  }
 }
